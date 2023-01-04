41c521cfeb946f3f68d989e69b328dc1b59f7891
Sidnei Farias <sidneijr.77@gmail.com>
2023-01-04 00:45:30 -0300

    Receiving data from user inputs, in Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,8 @@
 import { Play } from 'phosphor-react'
-import { useState } from 'react'
+
+// Hooks são funções que possuem o prefixo, "use" e acoplam uma funcionalidade em um componente existente (ex.: useState, useEffect, useReducer, etc.).
+import { useForm } from 'react-hook-form'
+
 import {
   CountdownContainer,
   FormContainer,
@@ -44,25 +47,47 @@ import {
  */
 
 export function Home() {
-  const [task, setTask] = useState('')
+  // const [task, setTask] = useState('')
+
+  // Retorna um objeto, com vários funções e variáveis disponíveis; portanto, destruturação é útil.
+  // "useForm" cria um novo formulário em minha aplicação. "Register" é um método que irá adicionar um input ao nosso formulário, dizendo quais campos terei no mesmo.
+  const { register, handleSubmit, watch } = useForm()
+
+  function handleCreateNewCycle(data: any) {
+    console.log(data)
+  }
+
+  // Consigo visualizar o valor de um campo específico, em tempo real.
+  const task = watch('task')
+  const isSubmitDisabled = !task
 
   return (
     <HomeContainer>
-      <form action="">
+      <form onSubmit={handleSubmit(handleCreateNewCycle)} action="">
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
           <TaskInput
             id="task"
             list="task-suggestions"
-            name="task"
             type="text"
             placeholder="Dê um nome para o seu projeto"
-            onChange={(e) => setTask(e.target.value.trim())} // atualiza o estado, a cada letra digitada ou removida.
+            // onChange={(e) => setTask(e.target.value.trim())} // atualiza o estado, a cada letra digitada ou removida.
+
+            /**
+             * 1) O "register" é uma função, onde passo um parâmetro para a mesma.
+             *
+             * 2) Imagine que a função "register" recebe o nome do input e retorna alguns métodos.
+             * Tais métodos são os mesmos que utilizamos para trabalhar com inputs no JavaScript. Ou seja, onChange, onBlur, onFocus.
+             * Então, nós utilizamos o spread operator para transformar cada um dos métodos presentes do retorno da função "register", em uma propriedade pra meu input.
+             *
+             * Ou seja, seria quase a mesma coisa que inserir onChange ou outros métodos diretamente.
+             */
+            {...register('task')}
             /**
              * Boa prática: definir nosso value do input, com o valor do estado. Se nosso estado "task" mudar, por uma origem que não seja a digitação do usuário,
              * eu também quero que o input seja atualizado visualmente, exibindo o novo valor.
              **/
-            value={task}
+            // value={task}
           />
 
           {/* Um datalist nada mais é do que uma lista de sugestões para um input. */}
@@ -81,6 +106,7 @@ export function Home() {
             step={5}
             min={5}
             max={60}
+            {...register('minutesAmount', { valueAsNumber: true })}
           />
 
           <span>minutos.</span>
@@ -95,7 +121,11 @@ export function Home() {
         </CountdownContainer>
 
         {/* O "!task.length" (ou task === ') indica "somente quando o input estiver vazio". */}
-        <StartCountdownButton disabled={!task.length} type="submit">
+        {/**
+         * Agora, eu preciso habilitar o botão de submit, baseado se a informação de task estará preenchida ou não.
+         * A função "watch" do useForm permite observar o campo desejdao.
+         */}
+        <StartCountdownButton disabled={isSubmitDisabled} type="submit">
           <Play size={24} />
           Começar
         </StartCountdownButton>
